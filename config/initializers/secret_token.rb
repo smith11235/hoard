64aff757cacb9c628596e0c71d8c609705d0c69e
smith11235 <smith11235@gmail.com>
2013-03-03 19:13:20 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoard::Application.config.secret_token = 'f9bdd06d12a53ee98b43b91f1fd06f822c56bf9140460dd21ae46d4b17ae2f63f0c29d33c4f366804beb927493f83ecbe86901fd5e10c50a67cb0c26fd02c33a'
